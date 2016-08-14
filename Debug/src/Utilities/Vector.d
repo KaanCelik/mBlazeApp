8a969f45c63d8a77367641d5fd5f1388903565c8
Kaan Celik <onurkaan.celik@gmail.com>
2016-08-14 12:29:02 +0300

    Ignored directories has been removed from the repo.

--- Debug/src/Utilities/Vector.d
+++
@@ -1,7 +0,0 @@
-src/Utilities/Vector.d: ../src/Utilities/Vector.c \
- ../src/Utilities/Vector.h \
- ../../mBlaze_v1.3_bsp/microblaze_0/include/xbasic_types.h
-
-../src/Utilities/Vector.h:
-
-../../mBlaze_v1.3_bsp/microblaze_0/include/xbasic_types.h:
